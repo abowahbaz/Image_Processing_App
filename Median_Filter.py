602fc903c0e1a91053e8dd4c0d6cb3acc5e23169
Yousef-Gaber11 <138793695+Yousef-Gaber11@users.noreply.github.com>
2024-12-12 13:51:55 +0200

    Added flexible size parameter for median filter and updated Median_Filter.

--- Median_Filter.py
+++ Median_Filter.py
@@ -2,28 +2,35 @@ from PIL import Image
 import numpy as np
 
 class Median_Filter:
-    def __init__(self, image_path):
-        self.image_path = image_path
-        self.image = Image.open(image_path).convert('L')
-        self.image_array = np.array(self.image)
+    def __init__(self):
+        pass
 
-    def median_filter_custom(self, size=3):
+    def median_filter_custom(self, image_array, size=3):
         pad = size // 2
-        padded_img = np.pad(self.image_array, pad, mode='constant', constant_values=0)
-        
-        filtered_img = np.zeros_like(self.image_array)
+        padded_img = np.pad(image_array, pad, mode='constant', constant_values=0)
+        filtered_img = np.zeros_like(image_array)
 
-        for i in range(self.image_array.shape[0]):
-            for j in range(self.image_array.shape[1]):
+        for i in range(image_array.shape[0]):
+            for j in range(image_array.shape[1]):
                 window = padded_img[i:i+size, j:j+size]
                 filtered_img[i, j] = np.median(window)
 
         return filtered_img
 
-    def show_filtered_image(self, size=3):
-        filtered_image = self.median_filter_custom(size)
-        filtered_image_pil = Image.fromarray(filtered_image)
-        filtered_image_pil.show()
+    def process_image(self, input_path, output_path, size=3):
+       
+        image = Image.open(input_path).convert('L')
+        image_array = np.array(image)
 
-run = Median_Filter(r'D:\PythonVS\IMG_Median\SP_Pics\2.jpeg')
-run.show_filtered_image(size=3)
+        filtered_image_array = self.median_filter_custom(image_array, size)
+
+        filtered_image = Image.fromarray(filtered_image_array)
+        filtered_image.save(output_path)
+
+
+run = Median_Filter()
+run.process_image(
+    input_path=r'D:\PythonVS\IMG_Median\SP_Pics\1.jpg',
+    output_path=r'D:\PythonVS\IMG_Median\SP_Pics\Median_filter_output.jpg',
+    # size=5
+)
