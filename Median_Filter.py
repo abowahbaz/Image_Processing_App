6ae7c2907135f2d3d82968193cc7791153b1086b
Mazen-Ghanaym <mznghanayem@gmail.com>
2024-12-12 15:45:04 +0200

    Modified Median Filter

--- Median_Filter.py
+++ Median_Filter.py
@@ -1,25 +1,36 @@
+"""
+    Module Documentation:
+    TODO
+"""
+
 from PIL import Image
 import numpy as np
 
-class Median_Filter:
+
+class MedianFilter:
+    """
+    Class Documentation:
+    """
     def __init__(self):
-        pass
+        """ Constructor Documentation """
+        self.image = None
 
     def median_filter_custom(self, image_array, size=3):
+        """ Function Documentation """
         pad = size // 2
-        padded_img = np.pad(image_array, pad, mode='constant', constant_values=0)
+        padded_img = np.pad(image_array, pad, mode="constant", constant_values=0)
         filtered_img = np.zeros_like(image_array)
 
         for i in range(image_array.shape[0]):
             for j in range(image_array.shape[1]):
-                window = padded_img[i:i+size, j:j+size]
+                window = padded_img[i : i + size, j : j + size]
                 filtered_img[i, j] = np.median(window)
 
         return filtered_img
 
     def process_image(self, input_path, output_path, size=3):
-       
-        image = Image.open(input_path).convert('L')
+        """ Function Documentation """ 
+        image = Image.open(input_path).convert("L")
         image_array = np.array(image)
 
         filtered_image_array = self.median_filter_custom(image_array, size)
@@ -27,10 +38,17 @@ class Median_Filter:
         filtered_image = Image.fromarray(filtered_image_array)
         filtered_image.save(output_path)
 
+    def original_image_size(self, image):
+        """ Function Documentation """
+        return image.size
 
-run = Median_Filter()
+    def new_image_size(self, image):
+        """ Function Documentation """
+        return image.size
+
+
+run = MedianFilter()
 run.process_image(
-    input_path=r'D:\PythonVS\IMG_Median\SP_Pics\1.jpg',
-    output_path=r'D:\PythonVS\IMG_Median\SP_Pics\Median_filter_output.jpg',
-    # size=5
+    input_path=r"test_image.png",
+    output_path=r"output_image.png",
 )
